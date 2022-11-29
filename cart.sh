05057dbd5819d1eefdf10c850adc4747c888bd63
Bhakti-19 <saraf.bhakti@gmail.com>
2022-11-29 00:57:31 -0600

    cart script

---
+++ cart.sh
@@ -0,0 +1,17 @@
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
+useradd roboshop
+ curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"
+ cd /home/roboshop
+ rm -rf cart
+ unzip -o /tmp/cart.zip
+ mv cart-main cart
+ cd cart
+ npm install
+
+ sed -i -e 's/REDIS_ENDPOINT/redis.Bhakti-19.online' -e 's/CATALOGUE_ENDPOINT/catalogue.Bhakti-19.online' /home/roboshop/cart/systemd.service
+
+mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service
+systemctl daemon-reload
+systemctl restart cart
+systemctl enable cart
