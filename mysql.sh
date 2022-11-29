0628e5cc026351298f9949d45e49243bae27ece9
Bhakti-19 <saraf.bhakti@gmail.com>
2022-11-29 17:36:48 -0600

    mysql script

--- mysql.sh
+++ mysql.sh
@@ -1,3 +1,8 @@
+if [ -z "$1"]; then
+  echo Input argument password is needed
+  exit
+  fi
+ROBOSHOP_MYSQL_PASSWORD=$1
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
 dnf module disable mysql -y
 
